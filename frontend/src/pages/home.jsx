c45f3e1fe1365579014c4dac91c76faffcca53d9
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-08-21 14:50:58 +0600

    fix: correct component naming and import paths in Home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import Navbar from '../components/navbar'
-import Header from '../components/header'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 
-const home = () => {
-
-
+const Home = () => {
   return (
     <div className='flex flex-col items-center
-     justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg bg-center'>
-      <Navbar  />
+     justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
+      <Navbar />
       <Header />
     </div>
   )
 }
 
-export default home
+export default Home

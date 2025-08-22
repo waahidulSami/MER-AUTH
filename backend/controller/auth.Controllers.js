40c30533dbbf79a7c211860427f6c6692817844f
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-08-22 15:36:38 +0600

    Update auth.Controllers.js

--- backend/controller/auth.Controllers.js
+++ backend/controller/auth.Controllers.js
@@ -28,7 +28,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true, sameSite: none
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
+      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in ms
     });
 
     const mailOptions = {
@@ -76,7 +76,7 @@ res.cookie("token", token, {
   httpOnly: true,
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
   secure: true,
-  maxAge: 7 * 24 * 60 * 60 * 1000,
+  maxAge: 7 * 24 * 60 * 60 * 1000
 });
 
 
@@ -102,7 +102,7 @@ export const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
    secure: true, sameSite: none,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      maxAge: 7 * 24 * 60 * 60 * 1000
     });
     return res.json({ success: true, message: "logged out" });
   } catch (error) {

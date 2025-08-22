28f4fc9f8773e84b8b48d6a48fe9a8724feb142a
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-08-22 15:34:44 +0600

    Update auth.Controllers.js

--- backend/controller/auth.Controllers.js
+++ backend/controller/auth.Controllers.js
@@ -72,11 +72,13 @@ export const login = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
-    res.cookie("token", token, {
-      httpOnly: true,
-  secure: true, sameSite: none
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
-    });
+res.cookie("token", token, {
+  httpOnly: true,
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
+  secure: true,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
 
     // sending welcome email
 
@@ -99,7 +101,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-   secure: true, sameSite: none
+   secure: true, sameSite: none,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.json({ success: true, message: "logged out" });
   } catch (error) {

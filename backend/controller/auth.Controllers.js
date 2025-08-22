8b574ddf0f1996c6d077044a776d8b5db4970726
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-08-22 15:25:19 +0600

    Update auth.Controllers.js

--- backend/controller/auth.Controllers.js
+++ backend/controller/auth.Controllers.js
@@ -75,7 +75,7 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure:false,
+     secure: process.env.NODE_ENV === "production", 
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
     });

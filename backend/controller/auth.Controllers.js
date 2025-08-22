ef6898799ccf5ec20974d922ad9f1e5adc40a945
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-08-22 15:29:52 +0600

    Update auth.Controllers.js

--- backend/controller/auth.Controllers.js
+++ backend/controller/auth.Controllers.js
@@ -27,8 +27,7 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true, sameSite: none
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
     });
 
@@ -75,8 +74,7 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-     secure: process.env.NODE_ENV === "production", 
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  secure: true, sameSite: none
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
     });
 
@@ -101,8 +99,7 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+   secure: true, sameSite: none
     });
     return res.json({ success: true, message: "logged out" });
   } catch (error) {

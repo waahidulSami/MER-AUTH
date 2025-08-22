d8c7c4b0519b1ebf964929017c6f4a50e25ffd02
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-08-22 14:56:23 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,7 @@ const app = express();
 
 
 const allowedOrigins = [
-  "http://localhost:5174",
-  "http://localhost:5173",
-  "http://localhost:5175",
+  "https://mer-auth-duwe.onrender.com"
 ];
 
 app.use(
